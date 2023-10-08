b02637c7cfa0f5b8919ac9faf0d1d28f69deb7f5
lim-yoona <1994238842@qq.com>
2023-10-08 12:53:10 +0800

    add Go-style documentation header

--- message.go
+++ message.go
@@ -1,17 +1,21 @@
 package msgpack
 
+// Imessage is an interface that definds a message,
+// which carries DataLen, MsgId and MsgData.
 type Imessage interface {
 	GetDataLen() uint32
 	GetMsgId() uint32
 	GetMsgData() []byte
 }
 
+// Message implements Imessage.
 type Message struct {
 	Id      uint32
 	DataLen uint32
 	Data    []byte
 }
 
+// NewMessage returns a message typed `*Message`.
 func NewMessage(id, datalen uint32, data []byte) *Message {
 	return &Message{
 		Id:      id,
@@ -20,12 +24,17 @@ func NewMessage(id, datalen uint32, data []byte) *Message {
 	}
 }
 
+// GetDataLen returns DataLen typed `uint32`.
 func (msg *Message) GetDataLen() uint32 {
 	return msg.DataLen
 }
+
+// GetMsgId returns Id typed `uint32`.
 func (msg *Message) GetMsgId() uint32 {
 	return msg.Id
 }
+
+// GetMsgData returns Data typed `[]byte`.
 func (msg *Message) GetMsgData() []byte {
 	return msg.Data
 }
